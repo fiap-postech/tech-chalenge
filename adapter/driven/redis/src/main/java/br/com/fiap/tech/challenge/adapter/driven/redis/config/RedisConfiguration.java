e87d45df3a0d3d22e00ab84c3249e8608c79e45d
Eberson Oliveira <eberson.olivera@gmail.com>
2023-06-17 17:19:33 -0300

    added redis module

---
+++ adapter/driven/redis/src/main/java/br/com/fiap/tech/challenge/adapter/driven/redis/config/RedisConfiguration.java
@@ -0,0 +1,7 @@
+package br.com.fiap.tech.challenge.adapter.driven.redis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfiguration {
+}
