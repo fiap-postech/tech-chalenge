ee68248cd3c837353c10fe59b2b9ce239d32351a
Eberson Oliveira <eberson.olivera@gmail.com>
2023-06-17 15:56:25 -0300

    created an adapter to get products through rest api

--- adapter/driver/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/ProductResource.java
+++ adapter/driver/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/ProductResource.java
@@ -1,23 +1,28 @@
 package br.com.fiap.tech.challenge.rest.resource;
 
 import br.com.fiap.tech.challenge.port.driver.QueryProductService;
+import br.com.fiap.tech.challenge.rest.resource.response.ProductResponse;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 @AllArgsConstructor
 public class ProductResource {
 
+    private ModelMapper mapper;
     private QueryProductService queryProductService;
 
     @GetMapping
-    public  now(){
-        return LocalDateTime.now().toString();
+    public List<ProductResponse> getAllAvailable(){
+        return queryProductService.allAvailable()
+                .stream()
+                .map(product -> mapper.map(product, ProductResponse.class))
+                .toList();
     }
-
 }
--- adapter/driver/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/request/Request.java
+++ adapter/driver/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/request/Request.java
@@ -1,12 +1,15 @@
-package br.com.fiap.tech.challenge.rest.dto;
+package br.com.fiap.tech.challenge.rest.resource.request;
 
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import java.io.Serial;
 import java.io.Serializable;
 
+
 @ToString
-public abstract class DTO implements Serializable {
+@EqualsAndHashCode
+public abstract class Request implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -529940882058735178L;
