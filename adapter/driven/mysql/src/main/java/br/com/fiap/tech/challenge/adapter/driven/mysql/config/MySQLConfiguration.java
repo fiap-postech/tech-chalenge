75e1a6f79a1ec310c725a3287b410c4e62607cc8
Eberson Oliveira <eberson.olivera@gmail.com>
2023-06-17 17:19:14 -0300

    renamed module database to mysql

---
+++ adapter/driven/mysql/src/main/java/br/com/fiap/tech/challenge/adapter/driven/mysql/config/MySQLConfiguration.java
@@ -0,0 +1,7 @@
+package br.com.fiap.tech.challenge.adapter.driven.mysql.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySQLConfiguration {
+}
