609d10a45c17bae5cf70b8219ad60651e2bc2db9
Eberson Oliveira <eberson.olivera@gmail.com>
2023-06-17 18:18:33 -0300

    fix mysql container in docker-compose.yml

--- infra/docker/mysql/script/setup.sql
+++ infra/docker/mysql/script/setup.sql
@@ -1,6 +1,6 @@
-create database tech_challenge character set utf8mb4 collate utf8mb4_0900_ai_ci;
+create database if not exists tech_challenge character set utf8mb4 collate utf8mb4_0900_ai_ci;
 
-create user 'sys_tech_challenge'@'%' identified with mysql_native_password by 'Nosorto7';
+create user if not exists 'sys_tech_challenge'@'%' identified with mysql_native_password by '6#233zJMNTjkQb#sgps#';
 
 grant all privileges on tech_challenge.* to 'sys_tech_challenge'@'%';
 
