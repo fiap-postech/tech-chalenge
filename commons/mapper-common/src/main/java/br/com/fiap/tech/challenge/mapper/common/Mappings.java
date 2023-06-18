a172072fe6d30cc8ef6a364852793b229de0aa37
Eberson Oliveira <eberson.olivera@gmail.com>
2023-06-18 11:29:40 -0300

    created a mapper utility class

---
+++ commons/mapper-common/src/main/java/br/com/fiap/tech/challenge/mapper/common/Mappings.java
@@ -0,0 +1,23 @@
+package br.com.fiap.tech.challenge.mapper.common;
+
+import br.com.fiap.tech.challenge.util.Moneys;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.javamoney.moneta.Money;
+import org.modelmapper.Converter;
+
+import java.math.BigDecimal;
+
+import static br.com.fiap.tech.challenge.util.MoneyConstants.CURRENCY_PRECISION;
+import static br.com.fiap.tech.challenge.util.MoneyConstants.CURRENCY_ROUNDING_MODE;
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Mappings {
+
+    public static Converter<Money, BigDecimal> priceToMoneyConverter() {
+        return ctx -> defaultIfNull(ctx.getSource(), Moneys.zero())
+                .getNumberStripped()
+                .setScale(CURRENCY_PRECISION, CURRENCY_ROUNDING_MODE);
+    }
+}
