15742d4040f7fd6c0ae4893ad4a25485d84432b7
Eberson Oliveira <eberson.olivera@gmail.com>
2023-06-26 11:50:01 -0300

    created endpoints to deal with enable, disable and find by category

--- adapter/driver/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/ProductResource.java
+++ adapter/driver/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/ProductResource.java
@@ -1,8 +1,7 @@
 package br.com.fiap.tech.challenge.rest.resource;
 
-import br.com.fiap.tech.challenge.port.driver.CreateProductService;
-import br.com.fiap.tech.challenge.port.driver.FindAllAvailableProductService;
-import br.com.fiap.tech.challenge.port.driver.FindProductByUUIDService;
+import br.com.fiap.tech.challenge.domain.ProductCategory;
+import br.com.fiap.tech.challenge.port.driver.*;
 import br.com.fiap.tech.challenge.rest.resource.request.CreateProductRequest;
 import br.com.fiap.tech.challenge.rest.resource.response.ProductResponse;
 import br.com.fiap.tech.challenge.rest.util.Pages;
@@ -15,9 +14,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static br.com.fiap.tech.challenge.rest.config.RestModelMapperConfiguration.REST_MODEL_MAPPER;
+import static br.com.fiap.tech.challenge.rest.util.Mappings.toProductResponse;
 
 @RestController
 @RequestMapping("/product")
@@ -25,38 +26,69 @@ public class ProductResource {
 
     private final ModelMapper mapper;
     private final FindAllAvailableProductService findAllAvailableProductService;
+    private final FindAllAvailableProductByCategory findAllAvailableProductByCategory;
     private final FindProductByUUIDService findProductByUUIDService;
     private final CreateProductService createProductService;
+    private final EnableProductService enableProductService;
+    private final DisableProductService disableProductService;
 
-    public ProductResource(@Qualifier(REST_MODEL_MAPPER) ModelMapper mapper, FindAllAvailableProductService findAllAvailableProductService, FindProductByUUIDService findProductByUUIDService, CreateProductService createProductService) {
+    public ProductResource(@Qualifier(REST_MODEL_MAPPER) ModelMapper mapper,
+                           FindAllAvailableProductService findAllAvailableProductService,
+                           FindAllAvailableProductByCategory findAllAvailableProductByCategory,
+                           FindProductByUUIDService findProductByUUIDService,
+                           CreateProductService createProductService,
+                           EnableProductService enableProductService,
+                           DisableProductService disableProductService) {
         this.mapper = mapper;
         this.findAllAvailableProductService = findAllAvailableProductService;
+        this.findAllAvailableProductByCategory = findAllAvailableProductByCategory;
         this.findProductByUUIDService = findProductByUUIDService;
         this.createProductService = createProductService;
+        this.enableProductService = enableProductService;
+        this.disableProductService = disableProductService;
     }
 
     @GetMapping
-    public ResponseList<ProductResponse> getAllAvailable(@ParameterObject Pageable pageable) {
-        return ResponseList.from(
-                findAllAvailableProductService.list(Pages.of(pageable)),
-                e -> mapper.map(e, ProductResponse.class)
-        );
+    public ResponseList<ProductResponse> getAllAvailable(@ParameterObject Pageable pageable,
+                                                         @RequestParam(required = false) ProductCategory category) {
+
+        var result = Optional.ofNullable(category)
+                .map(c -> findAllAvailableProductByCategory.list(c, Pages.of(pageable)))
+                .orElseGet(() -> findAllAvailableProductService.list(Pages.of(pageable)));
+
+        return ResponseList.from(result, e -> toProductResponse(mapper, e));
     }
 
     @GetMapping("/{uuid}")
-    public ProductResponse getByUUID(@PathVariable String uuid){
-        return mapper.map(
-                findProductByUUIDService.get(UUID.fromString(uuid)),
-                ProductResponse.class
+    public ProductResponse getByUUID(@PathVariable String uuid) {
+        return toProductResponse(
+                mapper,
+                findProductByUUIDService.get(UUID.fromString(uuid))
         );
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductResponse create(@RequestBody @Valid CreateProductRequest request){
-        return mapper.map(
-                createProductService.create(request.toDomain(mapper)),
-                ProductResponse.class
+    public ProductResponse create(@RequestBody @Valid CreateProductRequest request) {
+        return toProductResponse(
+                mapper,
+                createProductService.create(request.toDomain(mapper))
+        );
+    }
+
+    @PatchMapping("/{uuid}/enable")
+    public ProductResponse enable(@PathVariable String uuid) {
+        return toProductResponse(
+                mapper,
+                enableProductService.enable(findProductByUUIDService.get(UUID.fromString(uuid)))
+        );
+    }
+
+    @PatchMapping("/{uuid}/disable")
+    public ProductResponse disable(@PathVariable String uuid) {
+        return toProductResponse(
+                mapper,
+                disableProductService.disable(findProductByUUIDService.get(UUID.fromString(uuid)))
         );
     }
 }
