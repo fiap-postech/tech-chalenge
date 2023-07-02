5c922da2231ae61698787bc7fc24da676f1cf451
Eberson Oliveira <eberson.olivera@gmail.com>
2023-07-01 21:20:56 -0300

    #19 created order and ordem item domain models

--- application/src/main/java/br/com/fiap/tech/challenge/domain/Order.java
+++ application/src/main/java/br/com/fiap/tech/challenge/domain/Order.java
@@ -1,26 +1,75 @@
 package br.com.fiap.tech.challenge.domain;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.Accessors;
-import lombok.experimental.SuperBuilder;
 
 import java.io.Serial;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Accessors(fluent = true)
+@EqualsAndHashCode(callSuper = true)
 public class Order extends Entity {
 
     @Serial
     private static final long serialVersionUID = -9196907733871633595L;
 
-    private OrderStatus status;
+    @NotNull
+    private final OrderStatus status;
 
+    @NotNull
+    private final LocalDate date;
 
-    public Order(UUID uuid) {
+    @NotNull
+    @NotEmpty
+    @Valid
+    private final List<OrderItem> items;
+
+    @Builder(toBuilder = true)
+    public Order(@Builder.ObtainVia(method = "uuid") UUID uuid,
+                 @NotNull OrderStatus status,
+                 @NotNull LocalDate date,
+                 @NotNull List<OrderItem> items) {
         super(uuid);
+        this.status = status;
+        this.date = date;
+        this.items = items;
+    }
 
-        status = OrderStatus.PAID;
+    public Order prepared() {
+        return toBuilder()
+                .status(OrderStatus.MADE)
+                .build();
+    }
+
+    public Order delivered() {
+        return toBuilder()
+                .status(OrderStatus.DELIVERED)
+                .build();
+    }
+
+    public Order done() {
+        return toBuilder()
+                .status(OrderStatus.DONE)
+                .build();
+    }
+
+    public Order addItem(Product product, Quantity quantity) {
+        var item = OrderItem.builder()
+                .product(product)
+                .quantity(quantity)
+                .build();
+
+        var itemList = items();
+        itemList.add(item);
+
+        return toBuilder().items(itemList).build();
     }
 }
--- application/src/main/java/br/com/fiap/tech/challenge/domain/OrderItem.java
+++ application/src/main/java/br/com/fiap/tech/challenge/domain/OrderItem.java
@@ -1,4 +1,55 @@
 package br.com.fiap.tech.challenge.domain;
 
-public class OrderItem {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Accessors(fluent = true)
+@EqualsAndHashCode(callSuper = true)
+public class OrderItem extends ValueObject implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -2071891141308689952L;
+
+    @NotNull
+    @Valid
+    private final Product product;
+
+    @NotNull
+    @Valid
+    private final Quantity quantity;
+
+    @Builder(toBuilder = true)
+    public OrderItem(@NotNull Product product, @NotNull Quantity quantity) {
+        this.product = product;
+        this.quantity = quantity;
+
+        validate();
+    }
+
+    public Price unitPrice() {
+        return product().fullPrice();
+    }
+
+    public Discount unitDiscount() {
+        return product().discount();
+    }
+
+    public Price subTotal() {
+        return product().fullPrice().multiply(quantity());
+    }
+
+    public Discount discount() {
+        return product().discount().multiply(quantity());
+    }
+
+    public Price total() {
+        return product().price().multiply(quantity());
+    }
 }
