64c964aac5db69bd858a7140aece839b5454296b
Eberson Oliveira <eberson.olivera@gmail.com>
2023-07-02 01:53:09 -0300

    #21 added method to find purchase by uuid

--- adapter/driven/mysql/src/main/java/br/com/fiap/tech/challenge/adapter/driven/mysql/repository/PurchaseEntityRepository.java
+++ adapter/driven/mysql/src/main/java/br/com/fiap/tech/challenge/adapter/driven/mysql/repository/PurchaseEntityRepository.java
@@ -3,5 +3,10 @@ package br.com.fiap.tech.challenge.adapter.driven.mysql.repository;
 import br.com.fiap.tech.challenge.adapter.driven.mysql.model.PurchaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PurchaseEntityRepository extends JpaRepository<PurchaseEntity, Long> {
+
+    Optional<PurchaseEntity> findByUuid(String uuid);
+
 }
