7936322b45f107eeb52d3d4b188661b6d311e7b0
Eberson Oliveira <eberson.olivera@gmail.com>
2023-07-02 01:19:38 -0300

    #19 created output port for write purchase

---
+++ adapter/driven/mysql/src/main/java/br/com/fiap/tech/challenge/adapter/driven/mysql/service/PurchaseEntityService.java
@@ -0,0 +1,54 @@
+package br.com.fiap.tech.challenge.adapter.driven.mysql.service;
+
+import br.com.fiap.tech.challenge.adapter.driven.mysql.model.PaymentEntity;
+import br.com.fiap.tech.challenge.adapter.driven.mysql.model.PurchaseEntity;
+import br.com.fiap.tech.challenge.adapter.driven.mysql.model.PurchaseItemEntity;
+import br.com.fiap.tech.challenge.adapter.driven.mysql.repository.PaymentEntityRepository;
+import br.com.fiap.tech.challenge.adapter.driven.mysql.repository.PurchaseEntityRepository;
+import br.com.fiap.tech.challenge.domain.Purchase;
+import br.com.fiap.tech.challenge.port.driven.PurchaseWriterService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static br.com.fiap.tech.challenge.adapter.driven.mysql.config.MySQLModelMapperConfiguration.MYSQL_MODEL_MAPPER;
+
+@Service
+public class PurchaseEntityService implements PurchaseWriterService {
+
+    private final ModelMapper mapper;
+    private final PurchaseEntityRepository purchaseRepository;
+    private final PaymentEntityRepository paymentRepository;
+
+    public PurchaseEntityService(@Qualifier(MYSQL_MODEL_MAPPER) ModelMapper mapper,
+                                 PurchaseEntityRepository purchaseRepository,
+                                 PaymentEntityRepository paymentRepository) {
+        this.mapper = mapper;
+        this.purchaseRepository = purchaseRepository;
+        this.paymentRepository = paymentRepository;
+    }
+
+    @Override
+    @Transactional
+    public Purchase write(Purchase purchase) {
+        var purchaseEntity = savePurchase(purchase);
+
+        var paymentEntity = mapper.map(purchase.payment(), PaymentEntity.class);
+        paymentEntity.setPurchase(purchaseEntity);
+
+        paymentRepository.save(paymentEntity);
+
+        return mapper.map(purchaseEntity, Purchase.class);
+    }
+
+    private PurchaseEntity savePurchase(Purchase purchase) {
+        var purchaseEntity = mapper.map(purchase, PurchaseEntity.class);
+
+        purchase.items().stream()
+                .map(item -> mapper.map(item, PurchaseItemEntity.class))
+                .forEach(purchaseEntity::addItem);
+
+        return purchaseRepository.save(purchaseEntity);
+    }
+}
