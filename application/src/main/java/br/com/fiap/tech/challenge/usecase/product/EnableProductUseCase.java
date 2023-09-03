6e866aa690767d70a176e2214742a0e35a1a7466
Eberson Oliveira <eberson.olivera@gmail.com>
2023-09-03 18:14:08 -0300

    #46 moved enable and disable from hexagonal to clean

--- application/src/main/java/br/com/fiap/tech/challenge/usecase/product/EnableProductUseCase.java
+++ application/src/main/java/br/com/fiap/tech/challenge/usecase/product/EnableProductUseCase.java
@@ -3,5 +3,5 @@ package br.com.fiap.tech.challenge.usecase.product;
 import br.com.fiap.tech.challenge.enterprise.entity.Product;
 
 public interface EnableProductUseCase {
-    Product enable(Product product);
+    Product enable(String uuid);
 }
--- application/src/main/java/br/com/fiap/tech/challenge/usecase/product/impl/EnableProductUseCaseImpl.java
+++ application/src/main/java/br/com/fiap/tech/challenge/usecase/product/impl/EnableProductUseCaseImpl.java
@@ -1,17 +1,22 @@
 package br.com.fiap.tech.challenge.usecase.product.impl;
 
 import br.com.fiap.tech.challenge.enterprise.entity.Product;
+import br.com.fiap.tech.challenge.gateway.product.ProductReaderGateway;
 import br.com.fiap.tech.challenge.gateway.product.ProductWriterGateway;
 import br.com.fiap.tech.challenge.usecase.product.EnableProductUseCase;
 import lombok.RequiredArgsConstructor;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 class EnableProductUseCaseImpl implements EnableProductUseCase {
 
-    private final ProductWriterGateway writerService;
+    private final ProductReaderGateway readerGateway;
+    private final ProductWriterGateway writerGateway;
 
     @Override
-    public Product enable(Product product) {
-        return writerService.write(product.enable());
+    public Product enable(String uuid) {
+        var product = readerGateway.readById(UUID.fromString(uuid));
+        return writerGateway.write(product.enable());
     }
 }
