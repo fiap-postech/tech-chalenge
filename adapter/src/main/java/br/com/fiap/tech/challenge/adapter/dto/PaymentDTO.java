d0681c39cd40eb18690aae3d896e8103840bef85
Eberson Oliveira <eberson.olivera@gmail.com>
2023-09-04 20:52:43 -0300

    #50 fix serialization issues

--- adapter/src/main/java/br/com/fiap/tech/challenge/adapter/dto/PaymentDTO.java
+++ adapter/src/main/java/br/com/fiap/tech/challenge/adapter/dto/PaymentDTO.java
@@ -4,12 +4,16 @@ import br.com.fiap.tech.challenge.enterprise.enums.PaymentMethod;
 import br.com.fiap.tech.challenge.enterprise.enums.PaymentStatus;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 public class PaymentDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 312532141453893566L;
+
     private String id;
     private LocalDate date;
     private PaymentStatus status;
