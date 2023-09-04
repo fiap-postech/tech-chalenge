bc6f693d1e59b056ddc0744aeb07b0ed034db52d
Eberson Oliveira <eberson.olivera@gmail.com>
2023-09-04 20:41:54 -0300

    #50 created mappers

---
+++ drivers/rest/src/main/java/br/com/fiap/tech/challenge/rest/mapping/PaymentResponseMapper.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.challenge.rest.mapping;
+
+import br.com.fiap.tech.challenge.adapter.dto.PaymentDTO;
+import br.com.fiap.tech.challenge.rest.resource.response.PaymentResponse;
+import org.mapstruct.Mapper;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
+public interface PaymentResponseMapper {
+
+    PaymentResponse toResponse(PaymentDTO dto);
+}
