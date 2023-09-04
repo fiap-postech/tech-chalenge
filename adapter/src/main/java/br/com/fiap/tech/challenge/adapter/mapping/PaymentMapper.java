043e03c4706481571015764c9256f4b2e7d1ad8e
Eberson Oliveira <eberson.olivera@gmail.com>
2023-09-04 20:43:00 -0300

    #50 created purchase mapping

---
+++ adapter/src/main/java/br/com/fiap/tech/challenge/adapter/mapping/PaymentMapper.java
@@ -0,0 +1,30 @@
+package br.com.fiap.tech.challenge.adapter.mapping;
+
+import br.com.fiap.tech.challenge.adapter.dto.PaymentDTO;
+import br.com.fiap.tech.challenge.enterprise.entity.Payment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+import java.math.BigDecimal;
+
+@Mapper(uses = { CommonMapper.class })
+public interface PaymentMapper {
+
+    PaymentMapper INSTANCE = Mappers.getMapper(PaymentMapper.class);
+
+    @Mapping(target = "id", expression = "java(payment.uuid().toString())")
+    @Mapping(target = "amount", source = "payment", qualifiedByName = "mapAmountToDTO")
+    PaymentDTO toDTO(Payment payment);
+
+    @Mapping(target = "uuid", source = "id", qualifiedByName = "generateUuid")
+    @Mapping(target = "amount", source = "amount", qualifiedByName = "mapBigDecimalToMoney")
+    Payment toDomain(PaymentDTO dto);
+
+    @Named("mapAmountToDTO")
+    static BigDecimal mapAmountToDTO(Payment payment) {
+        return CommonMapper.map(payment.amount());
+    }
+
+}
