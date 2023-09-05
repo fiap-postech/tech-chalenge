1c50a4f2193449ce0a18d2443b6aff75fc80d3d7
Eberson Oliveira <eberson.olivera@gmail.com>
2023-09-04 21:53:03 -0300

    #50 used controller instead of use case in purchase resource

--- drivers/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/PurchaseResource.java
+++ drivers/rest/src/main/java/br/com/fiap/tech/challenge/rest/resource/PurchaseResource.java
@@ -1,18 +1,16 @@
 package br.com.fiap.tech.challenge.rest.resource;
 
+import br.com.fiap.tech.challenge.adapter.controller.purchase.FindAllPurchasesController;
+import br.com.fiap.tech.challenge.adapter.controller.purchase.FindPurchaseByUUIDController;
 import br.com.fiap.tech.challenge.adapter.controller.purchase.PaymentConfirmController;
+import br.com.fiap.tech.challenge.adapter.controller.purchase.UpdatePurchaseStatusController;
 import br.com.fiap.tech.challenge.enterprise.enums.PurchaseStatus;
 import br.com.fiap.tech.challenge.rest.mapping.PaymentConfirmMapper;
-import br.com.fiap.tech.challenge.rest.mapping.PurchaseMapperRest;
 import br.com.fiap.tech.challenge.rest.mapping.PurchaseResponseMapper;
 import br.com.fiap.tech.challenge.rest.resource.doc.PurchaseResourceDoc;
 import br.com.fiap.tech.challenge.rest.resource.request.PaymentConfirmRequest;
 import br.com.fiap.tech.challenge.rest.resource.response.PurchseResponse;
 import br.com.fiap.tech.challenge.rest.util.Pages;
-import br.com.fiap.tech.challenge.usecase.purchase.FindAllPurchasesUseCase;
-import br.com.fiap.tech.challenge.usecase.purchase.FindPurchaseByPaymentIdUseCase;
-import br.com.fiap.tech.challenge.usecase.purchase.FindPurchaseByUUIDUseCase;
-import br.com.fiap.tech.challenge.usecase.purchase.UpdatePurchaseUseCase;
 import br.com.fiap.tech.challenge.util.ResponseList;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -25,8 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/purchase")
 @RequiredArgsConstructor
@@ -35,30 +31,27 @@ public class PurchaseResource implements PurchaseResourceDoc {
     private final PaymentConfirmMapper paymentConfirmMapper;
     private final PurchaseResponseMapper purchaseResponseMapper;
 
-    private final PurchaseMapperRest purchaseMapperRest;
-    private final FindAllPurchasesUseCase findAllPurchasesUseCase;
-    private final FindPurchaseByUUIDUseCase findPurchaseByUUIDUseCase;
-    private final FindPurchaseByPaymentIdUseCase findPurchaseByPaymentIdUseCase;
-    private final UpdatePurchaseUseCase updatePurchaseUseCase;
+    private final FindAllPurchasesController findAllPurchasesController;
+    private final FindPurchaseByUUIDController findPurchaseByUUIDController;
+    private final UpdatePurchaseStatusController updatePurchaseStatusController;
     private final PaymentConfirmController paymentConfirmController;
 
     @GetMapping
     public ResponseList<PurchseResponse> getAllAvailable(@ParameterObject Pageable pageable) {
         return ResponseList.from(
-                findAllPurchasesUseCase.list(Pages.of(pageable)),
-                purchaseMapperRest::toPurchseResponse
+                findAllPurchasesController.list(Pages.of(pageable)),
+                purchaseResponseMapper::toResponse
         );
     }
 
     @GetMapping("/{uuid}")
     public PurchseResponse getByUUID(@PathVariable String uuid) {
-        return purchaseMapperRest.toPurchseResponse(findPurchaseByUUIDUseCase.get(UUID.fromString(uuid)));
+        return purchaseResponseMapper.toResponse(findPurchaseByUUIDController.get(uuid));
     }
 
     @PatchMapping("/{uuid}/{status}")
     public PurchseResponse updatePurchaseStatus(@PathVariable String uuid, @PathVariable PurchaseStatus status) {
-        var purchase = findPurchaseByUUIDUseCase.get(UUID.fromString(uuid));
-        return purchaseMapperRest.toPurchseResponse(updatePurchaseUseCase.updateStatus(purchase, status));
+        return purchaseResponseMapper.toResponse(updatePurchaseStatusController.update(uuid, status));
     }
 
     @PostMapping("/confirm")
