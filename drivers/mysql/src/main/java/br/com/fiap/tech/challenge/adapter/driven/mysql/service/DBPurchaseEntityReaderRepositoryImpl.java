8aa2a367aae28a28cddcb29e7eb458c0aa5603a4
Eberson Oliveira <eberson.olivera@gmail.com>
2023-09-04 22:48:22 -0300

    #50 implemented driver layer's repositories

--- drivers/mysql/src/main/java/br/com/fiap/tech/challenge/adapter/driven/mysql/service/DBPurchaseEntityReaderRepositoryImpl.java
+++ drivers/mysql/src/main/java/br/com/fiap/tech/challenge/adapter/driven/mysql/service/DBPurchaseEntityReaderRepositoryImpl.java
@@ -1,19 +1,19 @@
 package br.com.fiap.tech.challenge.adapter.driven.mysql.service;
 
-import br.com.fiap.tech.challenge.adapter.driven.mysql.mapping.PurchaseMapper;
+import br.com.fiap.tech.challenge.adapter.driven.mysql.mapping.DBPurchaseMapper;
 import br.com.fiap.tech.challenge.adapter.driven.mysql.model.PurchaseEntity;
 import br.com.fiap.tech.challenge.adapter.driven.mysql.repository.PurchaseEntityRepository;
-import br.com.fiap.tech.challenge.enterprise.entity.Purchase;
+import br.com.fiap.tech.challenge.adapter.dto.PurchaseDTO;
+import br.com.fiap.tech.challenge.adapter.repository.PurchaseReaderRepository;
 import br.com.fiap.tech.challenge.exception.ApplicationException;
-import br.com.fiap.tech.challenge.gateway.PurchaseReaderGateway;
 import br.com.fiap.tech.challenge.util.Page;
 import br.com.fiap.tech.challenge.util.ResponseList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.function.Function;
 
 import static br.com.fiap.tech.challenge.enterprise.enums.PurchaseStatus.FINISHED;
@@ -21,29 +21,25 @@ import static br.com.fiap.tech.challenge.enterprise.error.ApplicationError.PURCH
 import static java.util.Comparator.comparing;
 
 @Service
-public class PurchaseEntityReaderGateway implements PurchaseReaderGateway {
+@RequiredArgsConstructor
+public class DBPurchaseEntityReaderRepositoryImpl implements PurchaseReaderRepository {
 
-    private final PurchaseMapper purchaseMapper;
+    private final DBPurchaseMapper dbPurchaseMapper;
     private final PurchaseEntityRepository repository;
 
-    public PurchaseEntityReaderGateway(PurchaseMapper purchaseMapper, PurchaseEntityRepository repository) {
-        this.purchaseMapper = purchaseMapper;
-        this.repository = repository;
-    }
-
     @Override
-    public ResponseList<Purchase> readAll(Page page) {
+    public ResponseList<PurchaseDTO> readAll(Page page) {
         return readAll(page, repository::findAll);
     }
 
     @Override
-    public Purchase readById(UUID id) {
-        return repository.findByUuid(id.toString())
-                .map(entity -> entity.toDomain(purchaseMapper))
-                .orElseThrow(() -> new ApplicationException(PURCHASE_NOT_FOUND_BY_UUID, id.toString()));
+    public PurchaseDTO readById(String id) {
+        return repository.findByUuid(id)
+                .map(dbPurchaseMapper::toDTO)
+                .orElseThrow(() -> new ApplicationException(PURCHASE_NOT_FOUND_BY_UUID, id));
     }
 
-    private ResponseList<Purchase> readAll(Page page, Function<Pageable, org.springframework.data.domain.Page<PurchaseEntity>> reader) {
+    private ResponseList<PurchaseDTO> readAll(Page page, Function<Pageable, org.springframework.data.domain.Page<PurchaseEntity>> reader) {
         var result = reader.apply(PageRequest.of(page.number(), page.size()));
 
         return new ResponseList<>(
@@ -55,11 +51,11 @@ public class PurchaseEntityReaderGateway implements PurchaseReaderGateway {
         );
     }
 
-    private List<Purchase> getPurchases(org.springframework.data.domain.Page<PurchaseEntity> result) {
+    private List<PurchaseDTO> getPurchases(org.springframework.data.domain.Page<PurchaseEntity> result) {
         return result.getContent().stream()
-                .map(p -> p.toDomain(purchaseMapper))
-                .filter(p -> p.status() != FINISHED)
-                .sorted(comparing(p -> p.status().toString()))
+                .map(dbPurchaseMapper::toDTO)
+                .filter(p -> p.getStatus() != FINISHED)
+                .sorted(comparing(p -> p.getStatus().toString()))
                 .toList();
     }
 }
